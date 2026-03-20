ed778a28521f4d956e5528285cb5513812a8b553
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2026-03-20 13:28:18 +0100

    feat(display): board coordinates

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,32 @@
 import "./style.css";
 import { createInitialBoard } from "./chess/core/createInitialBoard";
 import { renderBoard } from "./chess/ui/renderBoard";
+import { renderCoordinates } from "./chess/ui/renderCoordinates";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <main class="app">
     <h1 class="title">Browser Chess TS</h1>
-    <div id="board" class="board"></div>
+
+    <div class="board-layout">
+      <div id="ranks" class="ranks"></div>
+
+      <div class="board-area">
+        <div id="board" class="board"></div>
+        <div id="files" class="files"></div>
+      </div>
+    </div>
   </main>
 `;
 
 const boardElement = document.querySelector<HTMLElement>("#board");
+const filesElement = document.querySelector<HTMLElement>("#files");
+const ranksElement = document.querySelector<HTMLElement>("#ranks");
 
-if (!boardElement) {
-  throw new Error("Board container not found");
+if (!boardElement || !filesElement || !ranksElement) {
+  throw new Error("Board coordinate containers not found");
 }
 
 const board = createInitialBoard();
 
 renderBoard(boardElement, board);
+renderCoordinates(filesElement, ranksElement);
